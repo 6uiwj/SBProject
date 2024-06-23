5e2c29b5f147acd1b450bbc4b7be81d25927401a
6uiwj <oterges00@gmail.com>
2024-06-23 17:57:50 +0900

    project created

---
+++ src/main/java/com/example/sbb/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @ResponseBody
+    @GetMapping("/hello")
+    public String Hello () {
+
+        return "hello Spring Boot Board";
+    }
+}
---
+++ src/main/java/com/example/sbb/HelloLombok.java
@@ -0,0 +1,12 @@
+package com.example.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    private String hello; //클래스에 속성 추가
+    private int lombok;
+
+}
