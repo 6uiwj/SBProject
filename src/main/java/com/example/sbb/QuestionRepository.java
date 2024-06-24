62280cd13719d485387e24a63d9734da38c4cb8a
6uiwj <oterges00@gmail.com>
2024-06-24 23:35:10 +0900

    240624 question&answer repository 생성, JUnit test 실행

---
+++ src/main/java/com/example/sbb/QuestionRepository.java
@@ -0,0 +1,21 @@
+package com.example.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * JpaRepository<엔티티, 기본키>
+ *  - JPA가 제공하는 인터페이스
+ *  - CRUD작업을 처리하는 메서드 내장
+ */
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    //'findBy + 엔티티의 속성명'으로 메서드를 작성하면 입력한 속성의 값으로 데이터 조회 가능
+
+    //SELECT * FROM QUESTION WHERE SUBJECT = 조건값
+    Question findBySubject(String subject);
+
+    //SELECT * FROM QUESTION WHERE SUBJECT = 조건값 AND WHERE CONTENT = 조건값
+    Question findBySubjectAndContent(String subject, String content);
+
+    //결과가 여러 건인 경우 리턴타입을 List로 작성
+    //ex: List<Question> findBy.....(   , ..  )..
+}
