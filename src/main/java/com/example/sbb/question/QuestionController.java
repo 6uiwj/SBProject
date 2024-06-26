60aba0d583f26c6fb79ef0b7989afc8e798a38e3
6uiwj <oterges00@gmail.com>
2024-06-26 23:07:29 +0900

    240626 서비스 생성, 질문 목록 링크 만들기, 상세페이지 매핑

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -11,7 +12,8 @@ import java.util.List;
 @RequiredArgsConstructor //final이 붙은 속성을 포험하는 생성자를 자동으로 만들어 줌 (자동의존주입 - 생성자 방식)
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    //private final QuestionRepository questionRepository; //서비스 생성 전
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) { //데이터를 저장할 Model 객체를 매개변수로 지정
@@ -19,9 +21,17 @@ public class QuestionController {
          * Model : 자바 객체와 템플릿 간 연결 고리 역할
          *          Model 객체를 따로 생성할 필요 없이 매개변수로 지정하면 스프링 부트가 자동으로 Model 객체 생성
          */
-        List<Question> questionList = this.questionRepository.findAll(); //질문 목록을 전부 조회해 questionList에 담음
+        //List<Question> questionList = this.questionRepository.findAll(); //질문 목록을 전부 조회해 questionList에 담음
+        List<Question> questionList = this.questionService.getList(); //서비스를 통해 질문 목록 조회
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    //@PathVariable : 경로변수? URL의 {id}를 변수로 값을 주기 위해서
+    // @PathVariable("A")와 @GetMapping(value = "/../../{A}")에서 A의 이름이 동일해야 함
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 
 }
