4fee2bb2763a8322d5a71e2a8b1c72b33e904a42
6uiwj <oterges00@gmail.com>
2024-07-10 22:50:44 +0900

    240710 - 페이징 처리2

--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -2,10 +2,12 @@ package com.example.sbb.question;
 
 import com.example.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor //생성자 방식 의존주입
@@ -13,12 +15,17 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
+
     /**
-     * 질문 목록 데이터 조회
+     * 질문 목록 데이터 조회 - 페이징 처리
+     * 페이지 번호를 입력받아 해당 페이지의 Page 객체를 리턴
      * @return
      */
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        //인수
+        // page: 조회할 페이지의 번호, 10: 한 페이지에 보여줄 게시물의 수
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
 
     /**
