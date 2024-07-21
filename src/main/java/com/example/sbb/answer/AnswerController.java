1f78b7d49817c827b859f842c2711fea88c7788f
6uiwj <oterges00@gmail.com>
2024-07-21 20:37:13 +0900

    240721 - 답변 수정 기능2

--- src/main/java/com/example/sbb/answer/AnswerController.java
+++ src/main/java/com/example/sbb/answer/AnswerController.java
@@ -6,13 +6,16 @@ import com.example.sbb.user.SiteUser;
 import com.example.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -26,7 +29,7 @@ public class AnswerController {
     private final UserService userService;
 
     /**
-     *
+     * 답변 생성
      * @param model
      * @param id @PathVariable 경로를 변수로 지정 "id"를 {id}에 대입
      * @param answerForm
@@ -47,5 +50,51 @@ public class AnswerController {
 
         this.answerService.create(question, answerForm.getContent(), siteUser); //답변 등록 서비스 추가
         return String.format("redirect:/question/detail/%s",id);
+    }
+
+    /**
+     * 답변 수정 메서드 - ID로 답변 조회,
+     * @param answerForm
+     * @param id
+     * @param principal
+     * @return
+     */
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) { //로그인 사용자와 답변 작성자가 일치하지 않을 때
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다."); //에러 출력
+        }
+        answerForm.setContent(answer.getContent()); //조회한 답변 데이터의 내용읗 AnswerForm 객체에 대입,
+        return "answer_form";
+    }
+
+    /**
+     * 답변 수정 메서드(post)
+     * @param answerForm
+     * @param bindingResult
+     * @param id
+     * @param principal
+     * @return
+     */
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        //AnswerForm 커맨드 객체 검증 결과에 에러가 있다면 다시 폼 반환
+        if( bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        //id로 답변 조회
+        Answer answer = this.answerService.getAnswer(id);
+        //글쓴이와 로그인한 사용자가 같지 않다면 에러
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        //일치한다면 서비스의 modify 메서드로 내용 수정
+        this.answerService.modify(answer, answerForm.getContent());
+        //수정 완료 후 리다이렉트 할 페이지
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 }
